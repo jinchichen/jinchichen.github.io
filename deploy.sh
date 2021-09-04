e31eacf2682cefaba0377f06aa11a402b41416d7
cjinchi <cjinchi@qq.com>
2021-09-04 09:36:09 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 bundle exec jekyll build
 cd _site
-mv index.html moments.html
-cp /home/jcchen/barber-jekyll/homepage/index.html ./index.html
-cp /home/jcchen/barber-jekyll/homepage/main.css ./assets/css/main.css
-echo www.chenjinchi.com > CNAME
+# mv index.html moments.html
+# cp /home/jcchen/barber-jekyll/homepage/index.html ./index.html
+# cp /home/jcchen/barber-jekyll/homepage/main.css ./assets/css/main.css
+# echo www.chenjinchi.com > CNAME
 git add -A
 git commit -m "update"
 git push origin master
