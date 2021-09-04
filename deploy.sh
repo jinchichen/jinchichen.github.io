3f47d56307358a8f05cc4f2e801a0c59f5c1807e
cjinchi <cjinchi@qq.com>
2021-09-04 09:47:00 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cd _site
 # mv index.html moments.html
 # cp /home/jcchen/barber-jekyll/homepage/index.html ./index.html
 # cp /home/jcchen/barber-jekyll/homepage/main.css ./assets/css/main.css
-# echo www.chenjinchi.com > CNAME
+echo www.22du.site > CNAME
 git add -A
 git commit -m "update"
 git push origin master
