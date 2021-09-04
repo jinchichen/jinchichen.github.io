9bf38833641a237053df71cdb8a8c54003fb22c5
cjc <jcchen.gm@gmail.com>
2021-09-04 10:12:42 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 bundle exec jekyll build
 cd _site
-# mv index.html moments.html
-# cp /home/jcchen/barber-jekyll/homepage/index.html ./index.html
-# cp /home/jcchen/barber-jekyll/homepage/main.css ./assets/css/main.css
 echo www.22du.site > CNAME
 git add -A
 git commit -m "update"
